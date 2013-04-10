7af4848677580ffd1649d90ff29534e5ddacc566
Christian Spanring <cspanring@gmail.com>
2013-04-10 14:10:00 -0300

    solution 4.js

--- 4.js
+++ 4.js
@@ -5,15 +5,17 @@
 var evenNumbers = []
 
 function addEvensOnly(num) {
-  if (num % 2 === 0)
-    console.log(num, 'is even, adding!')
-    evenNumbers.push(num)
+  // This is not Python.
+  if (num % 2 === 0) {
+    console.log(num, 'is even, adding!');
+    evenNumbers.push(num);
+  }
 }
 
-addEvensOnly(0)
-addEvensOnly(1)
-addEvensOnly(2)
-addEvensOnly(3)
-addEvensOnly(4)
+addEvensOnly(0);
+addEvensOnly(1);
+addEvensOnly(2);
+addEvensOnly(3);
+addEvensOnly(4);
 
-console.log('even numbers: ', evenNumbers)
+console.log('even numbers: ', evenNumbers);
