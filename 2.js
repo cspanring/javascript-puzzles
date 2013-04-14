215754ef6a61b56f2b9e5c03f55534073371dcff
Christian Spanring <cspanring@gmail.com>
2013-04-14 11:42:45 -0300

    adds comment to 2.js solution

--- 2.js
+++ 2.js
@@ -3,6 +3,7 @@
 
 // loops 10 times from 0 - 1000 in increments of 100
 for (var i = 0; i < 1000; i += 100) {
+  // Creating a private scope for each iteration
   waitFor(i, (function then() {
     console.log(i)
   })())
